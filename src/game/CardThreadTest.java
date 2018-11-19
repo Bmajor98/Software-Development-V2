a323099b106d3e3fd5b4074a8d626d57282b409e
toby1999 <taw211@exeter.ac.uk>
2018-11-19 11:16:00 +0000

    h

--- src/game/CardThreadTest.java
+++ src/game/CardThreadTest.java
@@ -2,18 +2,47 @@ package game;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
+
+
 import org.junit.Test;
 
 public class CardThreadTest {
+	
+	Card card1 = new Card(1);
+	Card card2 = new Card(2);
+	Card card3 = new Card(3);
+	Card card4 = new Card(4);
+	
+	
+	List<Card> listcards = new ArrayList<Card>();
+	
+	
+	
+	Player player = new Player(1, new ArrayList<Card>(), listcards);
+	
+	ArrayList<Player> players = new ArrayList<Player>();
+		players.add(player);
+	
+	
 
 	@Test
 	public void testDraw() {
-		fail("Not yet implemented");
+		
+		CardThread cardthread = new CardThread(0, players, null);
+		assertNotNull(players);
 	}
 
 	@Test
 	public void testDiscard() {
 		fail("Not yet implemented");
+	}
+	@Test
+	public void testCheckPack() {
+		
 	}
 
 }
