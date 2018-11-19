778e6a5bf33638bae3637fe055b4d7f092cc3ea2
Bmajor98 <34866809+Bmajor98@users.noreply.github.com>
2018-11-19 11:14:39 +0000

    as

---
+++ src/game/CardThreadTest.java
@@ -0,0 +1,19 @@
+package game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardThreadTest {
+
+	@Test
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiscard() {
+		fail("Not yet implemented");
+	}
+
+}
